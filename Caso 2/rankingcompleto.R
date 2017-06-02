58fcbdd3404cfd7ab43bb5b08e28499ad974142a
Guillermina Tlapale <guilletlapalet@gmail.comgit config --global user.email guilletlapalet@gmail.com>
2017-06-02 13:18:30 -0500

    Caso 2 corregido
    Caso 2. Función corregida

--- Caso 2/rankingcompleto.R
+++ Caso 2/rankingcompleto.R
@@ -33,7 +33,6 @@ rankingcompleto <- function(resultado, num = "mejor")  {
         dd <- which(s[,2]==x2[i] & s[,3] != "Not Available")
         if(num == "mejor") { num <- 1 }
         if(num == "peor") { num <- length(s[dd])}
-        #if(is.integer(num)==F ){stop("Solo enteros positivos, mejor y peor")}
         if(num > length(s[dd])) { 
             sol[i,1] <- "NA"
             sol[i,2] <- x2[i]
@@ -45,33 +44,39 @@ rankingcompleto <- function(resultado, num = "mejor")  {
             ss<-order(as.numeric(fnt))
             fns <- fn[ss]
             fn2<-fnt[ss]
+            x<- outcome[,7]
+            x1<- factor(x)
+            x2<-attr(x1,"levels")
+            
             res <-  which(fn2==fn2[ss[num]])
             
-            if (length(res)!=0){
-                
-                j <- 0
-                while (fn2[j+1] != fn2[num]){
-                    j <- j + 1
-                }
-                res <- which(fn2==fn2[num])
-                fn1<-order(fns[res])
-                
-                sol[i,1] <- fns[res[fn1[num-j]]]
-                sol[i,2] <- x2[i]
-            }
-            else{
-                sol[i,1] <- "NA"
-                sol[i,2] <- x2[i]
-            }
             
+            if (length(res)!=0){
+            
+            j <- 0
+            while (fn2[j+1] != fn2[num]){
+                j <- j + 1
+            }
+            res <- which(fn2==fn2[num])
+            fn1<-order(fns[res])
+            
+            sol[i,1] <- fns[res[fn1[num-j]]]
+            sol[i,2] <- x2[i]
         }
+        else{
+            sol[i,1] <- "NA"
+            sol[i,2] <- x2[i]
+        }
+        
     }
-    
-    rownames(sol) <- x2
-    colnames(sol) <- c("hospital","state")
-    format( sol[,1],justify="left")
-    sol
+}
+
+rownames(sol) <- x2
+colnames(sol) <- c("hospital","state")
+format( sol[,1],justify="left")
+sol
 }
 
 source("rankingcompleto.R")
-rankingcompleto("ataque",20),10)
+head(rankingcompleto("ataque",20),10)
+
